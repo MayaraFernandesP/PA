2b695e536922a71c886cb8c9e1e6d02f04c8d996
Aluno <aluno@etec.sp.gov.br>
2023-10-17 22:53:27 -0300

    Aula-17-10-23

---
+++ PA/Aula-17-10-2023/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Aula_17_10_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+            string operador;
+
+            Console.Write("Digite o primeiso número: ");
+            n1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o operador: ");
+            operador = Console.ReadLine();
+
+            switch(operador)
+            {
+                case "+":
+                    Console.WriteLine($"A soma é: {n1 + n2}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"A subtração é: {n1 - n2}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"A multiplicação é: {n1 * n2}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"A divisão é: {n1 / n2}");
+                    break;
+
+                default:
+                    Console.WriteLine("Operador Inválido");
+                    break;
+            }
+
+        }
+    
+    }
+}
